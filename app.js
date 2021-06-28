d828dbaab4fc1a1b6363bf4b85ee757e466fc83c
HemaVyas <Hema@hkls-mbp.lan>
2021-06-28 19:30:45 -0400

    HW part 2 complete

--- app.js
+++ app.js
@@ -1,83 +1,74 @@
 // bring in data
 d3.json("samples.json").then((data)=> {
 
-var samples = data.names;
-// console.log(samples);
-var test = Object.entries(data.samples).map(row => row);
-var filterTest = test.filter(row => row.id === "941");
-// console.log(filterTest); //This pulls data but I can't seem to do anything with it.  Always 'undefined'
+    var samples = data.names;
 
-//Trying to figure out how to pull data!!!
-var xtest = Object.entries(filterTest).map(entry=> entry[1]);
-//   undefined  var x = Object.entries(filterTest[1]);
-//var testing = x[0].otu_ids;
-// var ytest = x.otu_ids;
-// var x = filterTest.otu_ids.values;
-//Different way   var x = object.value(filterTest.otu_ids);
-// var y = filterTest.samples_values.values;
-var testing = data.samples.filter(row =>row.id === "941")[0].otu_ids;
-var otuValues = data.samples.filter(row =>row.id === "941")[0].sample_values;
-// console.log(xtest);
-// console.log(ytest);
-var testing10 = testing.slice(0,10);
-var otuValues10 = otuValues.slice(0,10);
-// console.log(testing10);
-// console.log(otuValues10);
-var newNames = testing10.map(function(el){
-    return 'OTU ' + el;
-});
+    var testing = data.samples.filter(row =>row.id === "940")[0].otu_ids;
+    var otuValues = data.samples.filter(row =>row.id === "940")[0].sample_values;
 
-console.log(newNames);
+    var testing10 = testing.slice(0,10);
+    var otuValues10 = otuValues.slice(0,10);
 
-var demographics = data.metadata;
-//console.log(demographics);
-var demo1 = Object.entries(demographics[0]);
-// console.log(demo1);
-// alert(Object.entries(demo1));
-var demoLoc = d3.selectAll("panel-heading").node();
-// demo1.forEach(row => {
-//         var h5 = demoLoc.append("h3");
-//         console.log(row)
-//         Object.values(row).forEach((value)=> {
-//             h5.append("h3").text(value);
-//         });
-// });
+    var newNames = testing10.map(function(el){
+        return 'OTU ' + el;
+    });
+    var reverseNames = newNames.reverse();
+    var reverseOTU = otuValues10.reverse();
+    console.log(newNames);
 
-buildDropDown(samples);
+
+    d3.json("samples.json").then(({metadata, samples})=> {
+        demo = metadata.filter(obj =>obj.id == 940)[0];
+        sample = samples.filter(obj => obj.id == 940)[0];
+
+        d3.select('.panel-body').html('');
+        Object.entries(demo).forEach(([key,val]) => { 
+            d3.select('.panel-body').append('h5').text(key.toUpperCase() + ' : ' + val);});
+
+        console.log(demo, sample);
+    });
+
+    buildDropDown(samples);
 //Build initial plot
-data = [{ 
-    x: newNames,
-    y: otuValues10,
-    type: "bar" }];
+    data = [{ 
+        x: reverseOTU ,
+        y: newNames,
+        type: "bar" ,
+        orientation: "h"}];
 
     var BAR = d3.selectAll("#bar").node();
     var layout = {
-      title: "Top 10 Bacteria Cultures Found"
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis : {title: "Value"},
+      yaxis : {title: "OTU ID"}
         };
 
     Plotly.newPlot(BAR, data, layout);
 
+
+    // Create a bubble chart that displays each sample.
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    // Use otu_labels for the text values.
+    //Basic bubble
+    trace = [{
+        x: otu_ids,
+        y:otu_labels,
+        marker: {
+            color: otu_ids,
+            size: samples_values,
+        },
+        mode: 'markers',
+        text: otu_labels, 
+    }]
+    layout = {
+        title: "",
+        xaxis : {title: ""}
+    }
+    Plotly.plot("bubble",trace,layout)
 });
-
-// function init() {
-// data = [{ 
-//     // x: ["OTU 1167", " OTU 2859", "O482", "O2264", "O41", "O1189", "O352", "O189", "O2318", "O1977"],
-//     // y: [163, 126, 113, 78, 71, 51, 50, 47, 40, 40],
-//     // type: "bar" }];
-
-//     x: testing10,
-//     y: otuValues10,
-//     type: "bar" }];
-
-
-//     var BAR = d3.selectAll("#bar").node();
-//     var layout = {
-//       title: "Top 10 Bacteria Cultures Found"
-//         };
-
-//     Plotly.newPlot(BAR, data, layout);
-
-// }
 
 
 function buildDropDown(samples){
@@ -98,27 +89,31 @@ function makeBar(){
     var BAR = d3.selectAll("#bar").node();
     console.log(sampleID);
 
-    var x = [];  //samples.otu_ids  slice(0,10)  with === sampleID 
-    var y = [];  //samples.out_values slice(0,10) with === sampleID
-//UPDATE plot here when change occurs
-        // Plotly.restyle(CHART, "x", [x]);
-        // Plotly.restyle(CHART, "y", [y]);
-
+//Make demographics update
+    d3.json("samples.json").then(({metadata, samples})=> {
+    demo = metadata.filter(obj =>obj.id == sampleID)[0];
+    sampleNames = samples.filter(obj => obj.id == sampleID)[0].otu_ids;
+    otuValues = samples.filter(obj => obj.id == sampleID)[0].sample_values;
+//Demographic chart
+    d3.select('.panel-body').html('');
+    Object.entries(demo).forEach(([key,val]) => { 
+        d3.select('.panel-body').append('h5').text(key.toUpperCase() + ' : ' + val);});
     
-    // not working var testing = test.filter(row =>row.id === sampleID)[0].otu_ids;
+    var sampleName10 = sampleNames.slice(0,10);
+    var otuValues10 = otuValues.slice(0,10);
 
-    // // Integer to string? Still doesn't work
-    // var stringNum = sampleID.toString()
-    // console.log(stringNum);
+    var newNames = sampleName10.map(function(el){
+        return 'OTU ' + el;
+    });
+    var reverseNames = newNames.reverse();
+    var reverseOTU = otuValues10.reverse();
+    console.log(reverseNames); 
 
-    console.log(samples);
+    var x = reverseOTU;  
+    var y = reverseNames;  
+    
+        Plotly.restyle(BAR, "x", [x]);
+        Plotly.restyle(BAR, "y", [y]);
 
-
-
-
-       
-
-
+    });
 };
-
-// init ();
